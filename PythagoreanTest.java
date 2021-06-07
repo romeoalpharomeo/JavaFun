5e54812b1e401c918bc802687848b8e024b8baa6
romeoalpharomeo <ryanrey0333@gmail.com>
2021-06-07 15:11:16 -0400

    pythagorean complete

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main (String[] args) {
+        Pythagorean test = new Pythagorean();
+        double testArgs = test.calculateHypotenuse(6, 13);
+        System.out.println(testArgs);
+    }
+}
