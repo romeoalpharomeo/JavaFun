53a338e8c24df187bdba61af07a042743b8bc04c
romeoalpharomeo <ryanrey0333@gmail.com>
2021-06-07 16:55:00 -0400

    fizzbuzz complete

---
+++ FizzBuzz.java
@@ -0,0 +1,24 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        // fizzbuzz logic here
+        String stringToPrint = "";
+
+        if (number % 3 != 0 && number % 5 != 0) {
+            stringToPrint += number;
+        }
+        if (number % 3 == 0) {
+            stringToPrint += "Fizz";
+        }
+        if (number % 5 == 0) {
+            stringToPrint += "Buzz";
+        }
+        
+        return stringToPrint;
+
+
+        // else {
+        //     System.out.println(number);
+        // }
+    }
+}
+
---
+++ FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+    public static void main (String[] args) {
+        FizzBuzz test = new FizzBuzz();
+        String testArg = test.fizzBuzz(17);
+        System.out.println(testArg);
+        
+    }
+}
